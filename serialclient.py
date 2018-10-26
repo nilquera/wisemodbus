2f8fa96d585062eadc587f6ab60101246743ddbc
nilquera <nlquera97@gmail.com>
2018-10-26 10:33:51 +0200

    simple request

--- serialclient.py
+++ serialclient.py
@@ -1,15 +1,24 @@
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 import pymodbus.exceptions as exceptions
 import math
+
 usb = "/dev/ttyUSB0"
-client = ModbusClient(method='rtu', port=usb, stopbits=1, bytesize=8, baudrate=9600, timeout=1)
-client.connect()
+
+try:
+    client = ModbusClient(method='rtu', port=usb, stopbits=1, bytesize=8, baudrate=9650, timeout=1)
+    client.connect()
+except Exception as e:
+    print(e)
+
+
 
 rr = client.read_input_registers(address=514, count=2, unit=1)
-escale = client.read_input_registers(address=513, count=1, unit=1).registers[0]
+print(rr)
 
-high = rr.registers[0]
-low = rr.registers[1]
-raw_value = high*65536 + low
-value = raw_value * math.pow(10, escale-6)
-print(value)
+#escale = client.read_input_registers(address=513, count=1, unit=1).registers[0]
+
+#high = rr.registers[0]
+#low = rr.registers[1]
+#raw_value = high*65536 + low
+#value = raw_value * math.pow(10, escale-6)
+#print(value)
